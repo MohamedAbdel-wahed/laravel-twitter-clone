7807e828f44878e84524963b335829ab6acb8726
Mohamed <webdeveloper.int99@gmail.com>
2020-09-22 23:21:04 +0200

    added some changes to config/filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,8 +51,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => public_path('uploads'),
-            'url' => env('APP_URL').'/public',
-            'visibility' => 'uploads',
+            'url' => env('APP_URL').'/uploads',
+            'visibility' => 'public',
         ],
 
         's3' => [
